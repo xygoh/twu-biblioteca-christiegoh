91f656cd93459e3eb50b53ec011ee7face8d716b
XY Goh <xygoh@192-168-1-105.tpgi.com.au>
2018-07-03 15:29:52 +1000

    Welcome Message Test and functionality

---
+++ test/com/twu/biblioteca/Tests.java
@@ -0,0 +1,35 @@
+package com.twu.biblioteca;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+public class Tests {
+    @Rule
+    public final SystemOutRule sor = new SystemOutRule().enableLog();
+
+    // Welcome Message
+    @Test
+    public void testWelcomeMessage(){
+        // check log for welcome message
+        BibliotecaApp.main(new String[] {"a1","a2","a3"});
+        Assert.assertEquals( " ~ Welcome to Biblioteca ~ \n",sor.getLog());
+    }
+
+    // List Books
+    // Book Details
+    // Main Menu
+    // Invalid Menu Option
+    // Quit
+    // Checkout book
+    // Successful checkout
+    // Unsuccessful Checkout
+    // Return Book
+    // Successful Return
+    // Unsuccessful Return
+
+}
